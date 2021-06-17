05719356e054e2de2f2c2856a8b081ee8790969b
muthu kumaran <tikumaran@gmail.com>
2021-06-18 01:55:59 +0530

    TaskForDD

---
+++ 17Jun21 Task/EFDBDDList/Controllers/DepartmentController.cs
@@ -0,0 +1,34 @@
+﻿using EFDBDDList.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFDBDDList.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        
+        public ActionResult Index()
+        {
+            DepartmentModel dm = new DepartmentModel();
+            dm.Departments = dm.PopulateDepartments();
+            return View(dm);
+        }
+        [HttpPost]
+        public ActionResult Index(DepartmentModel department,string action)
+        {
+            
+            department.Departments = department.PopulateDepartments();
+            var selectedItem = department.PopulateDepartments().Find(p => p.Value == department.DepartmentID.ToString());
+            if (selectedItem != null && action == "Submit")
+            {
+                department.dt = department.DisplayEmployees(Convert.ToInt32(selectedItem.Value));
+            }
+            return View(department);
+        }
+    }
+}
---
+++ 17Jun21 Task/EFDBDDList/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFDBDDList.MvcApplication" Language="C#" %>
